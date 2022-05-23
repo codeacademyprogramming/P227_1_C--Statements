8797f1bca7ebdba893bba5fe87274c923a739ae1
eaghayev <eaghayevv@gmail.com>
2022-05-23 22:20:46 +0400

    project added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variable Cases
+            //camel case
+            //int avgStudentPoint
+
+            //pascal case
+            //int AvgStudnetPoint
+
+            //snake case
+            //int avg_student_point
+
+            //kebab case
+            //int avg-student-point
+            #endregion
+
+            int number = 4;
+
+            if (number>0)
+            {
+                Console.WriteLine("musbetdir");
+            }
+            else if(number<0)
+            {
+                Console.WriteLine("menfidir");
+            }
+            else 
+            {
+                Console.WriteLine("sifirdir");
+            }
+
+
+            int weekDay = 4;
+
+            //if(weekDay == 1)
+            //{
+            //    Console.WriteLine("Bazar ertesi");
+            //}
+            //else if(weekDay == 2)
+            //{
+            //    Console.WriteLine("Cersenbe axsami");
+            //}
+            //else if(weekDay == 3)
+            //{
+            //    Console.WriteLine("Cersenbe");
+            //}
+            //else if(weekDay == 4)
+            //{
+            //    Console.WriteLine("Cume axsami");
+            //}
+            //else if (weekDay == 5)
+            //{
+            //    Console.WriteLine("Cume");
+            //}
+            //else if (weekDay == 6)
+            //{
+            //    Console.WriteLine("Senbe");
+            //}
+            //else if (weekDay == 7)
+            //{
+            //    Console.WriteLine("Bazar");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Heftenin gunu yanlisdir");
+            //}
+
+
+            switch (weekDay)
+            {
+                case 1:
+                    Console.WriteLine("Bazar ertesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Cersenbe axsami");
+                    break;
+                case 3:
+                    Console.WriteLine("Cersenbe");
+                    break;
+                case 4:
+                    Console.WriteLine("Cume axsami");
+                    break;
+                case 5:
+                    Console.WriteLine("Cume");
+                    break;
+                case 6:
+                    Console.WriteLine("Senbe");
+                    break;
+                case 7:
+                    Console.WriteLine("Bazar");
+                    break;
+                default:
+                    Console.WriteLine("Heftenin gunu yanlisdir!");
+                    break;
+
+            }
+
+
+
+
+            int[] numbers = { 10, 7,34, 56, 33,98 };
+
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    if(numbers[i]%2==0)
+            //        Console.WriteLine(numbers[i]);
+            //}
+
+            int index = 0;
+
+            while (index<numbers.Length)
+            {
+                if(numbers[index]%2==0)
+                    Console.WriteLine(numbers[index]);
+
+                index++;
+            }
+
+            //string input = Console.ReadLine();
+
+            //while(input != "salam")
+            //{
+            //     input = Console.ReadLine();
+            //}
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+            } while (input!="salam");
+
+            Console.WriteLine("Poses bitdi");
+
+
+            Console.WriteLine("items:");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Yasi daxil edin:");
+            var ageStr = Console.ReadLine();
+            byte age = Convert.ToByte(ageStr);
+
+            int bornYear = 2022 - age;
+            Console.WriteLine(bornYear);
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            #region task1
+
+            //Verilmis siyahidaki en boyuk ededi tapan proqram
+
+            //int[] numbers = { 45, 3, 23, 6 };
+            //int[] numbers = new int[5];
+
+            //numbers[0] = 55;
+            //numbers[1] = 36;
+
+            int[] numbers = new int[6] { 10, 45, 77, 33, 25, 66 };
+
+            int biggest = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (biggest < numbers[i])
+                {
+                    biggest = numbers[i];
+                }
+            }
+
+            Console.WriteLine(biggest);
+            #endregion
+
+
+            //Verilmis ededler siyahisindaki 3ve 7e bolunen ededlerin ededi
+            //ortasini tapan proqram yazirsiniz
+
+            #region task2
+
+            double[] numbers2 = { 45, 10, 50, 66, 21, 42 };
+
+            double sum = 0;
+            int count = 0;
+
+            for(int i = 0; i < numbers2.Length; i++)
+            {
+                if(numbers2[i]%3==0 && numbers2[i] % 7 == 0)
+                {
+                    sum += numbers2[i];
+                    count++;
+                }
+            }
+
+            if(count == 0)
+                Console.WriteLine("3-e ve 7-e bolunen yoxdur!");
+            else
+            {
+                double result = sum / count;
+                Console.WriteLine(result);
+            }
+
+            //byte num = 45;
+            //int a = num;
+
+            //double b = a;
+
+
+            //int num1 = 45;
+            //double num2 = 2;
+
+            //double r = num1 / num2;
+
+            string name = "Hello, P227!";
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            #endregion
+
+            #region task3
+            //Verilmis yazidaki a herflerinin sayini tapan proqram
+            //yazini console - dan daxil edirsiniz
+
+            Console.WriteLine("Yazini daxil edin:");
+            string text = Console.ReadLine();
+
+            int letterCounter = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == 'a')
+                    letterCounter++;
+            }
+
+            //string output = text+ " sozundeki a herflerinin sayi: " + letterCounter;
+
+            Console.WriteLine(text + " sozundeki a herflerinin sayi: " + letterCounter);
+
+            #endregion
+
+            #region task4
+            //Daxil edilmis yazida 1 reqeminin olub olmamasini tapan proqram
+
+            Console.WriteLine("Metni daxil edin:");
+            string text2 = Console.ReadLine();
+
+            bool has1 = false;
+            for (int i = 0; i < text2.Length; i++)
+            {
+                if(text2[i] == '1')
+                {
+                    has1 = true;
+                    break;
+                }
+            }
+
+            if(has1)
+                Console.WriteLine("Daxil edilmis metnde 1 deyeri var");
+            else
+                Console.WriteLine("Daxil edilmis metnde 1 deyeri yoxdur!");
+
+
+            //Verilmis yazinin icerisinde reqem olub olmadigini tapan proqram
+            char[] digits = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+
+            #endregion
+
+        }
+    }
+}
